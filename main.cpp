e7e8c160476ab80062d005b0cd69c982c2149044
Tianyu Zhu <tian.tian098@gmail.com>
2015-01-27 23:14:43 -0500

    Allow query strings in argument section

--- main.cpp
+++ main.cpp
@@ -1,15 +1,30 @@
 #include <QApplication>
 #include <QtWebEngineWidgets>
+#include <QStringList>
+#include <cassert>
 
 static const auto homepage = "http://www.google.com";
 
+QString getInitialUrl(QStringList args) {
+    if (args.size() < 2) return homepage;
+
+    if (args.size() == 2) return args.at(1);
+
+    assert(args.size() > 2);
+    QStringList query{ args };
+    query.pop_front();
+    query.pop_front();
+
+    return QString(args.at(1))
+        .replace("%s", "%1")
+        .arg(query.join(' '));
+}
+
 int main(int argc, char *argv[])
 {
-    const auto initialUrl = argc < 2? homepage : argv[1];
-
     QApplication app{argc, argv};
     QWebEngineView webEngine{};
-    webEngine.load(QUrl::fromUserInput(initialUrl));
+    webEngine.load(QUrl::fromUserInput(getInitialUrl(app.arguments())));
     webEngine.show();
 
     return app.exec();
